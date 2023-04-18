19f766124568c2484b9d6c1098edfa640cf74d2a
iocodes20 <91490063+iocodes20@users.noreply.github.com>
2023-04-18 05:05:03 +0330

    Update autoscript.sh

--- autoscript.sh
+++ autoscript.sh
@@ -108,13 +108,6 @@ bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/
 read -p "Enter remote password: " remote_pass
 cd /root/
 
-sshpass -p 'qrsUFX238' scp root@157.90.0.90:/root/lhsconfigfiles/iran/config.json /usr/local/etc/v2ray/
-
-sshpass -p 'qrsUFX238' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@157.90.0.90:/root/lhsconfigfiles/server /root/
-sshpass -p 'qrsUFX238' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@157.90.0.90:/root/lhsconfigfiles/.env /root/
-sshpass -p 'qrsUFX238' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@157.90.0.90:/root/lhsconfigfiles/web.service /lib/systemd/system/web.service
-
-
 v2ray_Service=$(
     cat <<EOF
 [Unit]
@@ -200,15 +193,6 @@ echo "Welcome to $(hostname)" | sudo tee /etc/issue.net
 mkdir -p /home/lhs/.ssh
 
 touch /home/lhs/.ssh/authorized_keys
-
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCah7B06XyodjT00Bxde7IR9gcYz2GEremDTzQ0bi9DU3WtykaUiV9oR3xDXJj73RzLldrtt3Rc0XpvnJNRqnPfc6soPBgMhCoiG5Sx18Ivk2bGULJlg5oxcq2SuawjuEZJrRrKMfqhA967c0cpbvlejkk3ycOqm3O8vZNx9fn40gUX9C5jzPWp1KfJyzY+x5m/bNgjXQbKBHlDP3I+AASgF7qNjGUHzoBNN4EGpS0oMYIMdlRnFkzhfkiBWFNxZZJDMVeYWAb7MmLt22HxnVPGw3L0G/tPnQ1XVqwke/bfTunRZLigi/few/Clz0vQS+p+UaZRmQutSh9lAlgGGin3gzheXfbxyzdUA+qFBN056fjJ/tCw9poJYftkFwIspGf5QozeCAdaZ6XynEUZr4PmaJkfGJqIpfvoettIhoHgjingAiLmb74mCoQtugfCHl/t1MsTCto7b1ng84c/JVd5R4zOIzwVM0Fl3SGFrZCLRfHgg4ALBiure/mnDN150AE= joniur@itsjoniur" >> /home/lhs/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCHLxb6MSI1pRDQEo6a2uBOaYB/Ye2wjXyT9OFjFdp3DcZPdwWJ3dag/fpNBTHoscjmMDBVOvqy46f27KdD0CbVLkBAu2fVeP9eQ7xXAPWAhF2J94zrNH7Xs8wxI5b2pfT06OrtYwe/AQnCe9oQdJV5gcS7Hfys0HoiGgQks9tppyZVdCJg0vEpDn0RV6cHZDXhMfmJYp6s/H3ku+KWZtVCE0kBQTkQu8Lb3ITUuGYTBeMWY6XNvPKh1ETlQb/VIV463B/kiDc9Qz3NRQGsXKTbm73DrXXJzak6Q5tq02TZEaqffu5SYLO7xsRY6STNJe+r19hZo6ih9kznGrmrKWdn rsa-key-20230114" >> /home/lhs/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCZcRAEoWP6nBjg/jA4BxxQy7DZm4uqeSGU/e7ujEoI1VzFEcfPK2hUkqGdwoqHU5p6u03PI8eUyMf27qqB/DFV6LejCV6qM3Hr6qX7XmvVfzmQ7ejwdUIOerAZiTBefv7HejK3mXoF3E9QZ++/Ybj46oap0pDz4nmpf2RkTVXamWzALERf9HOoYMpA4NcBSNsvvWgPsJ/8ttf6LZEEVCwzO8aWvmaZUGhoYn75qvD9FHLhLkpkafHSHazBDTrnPjcYdDvwwSnb8pip9mOGjdJ18dARbt6rAnOgvMe1GC5GYM7qm5hx2EcCqbAeS10eTEeYNjsOmBC5ZOPgNTkvbs24n6YsyFcCZNcVDn4buCDjiWshwY5llFQAC2duS4zk+CN0DOBeplSMWpt6VQlPHwFv9sYtiAMPxDCEPLiu+iitpv6goGhNShwbesRfCIYalhv3KycF3Fpsctl6854BgRo/rAXLzSsGPySNi1qIqVaWqcv6U92wITEcF243A0AZ3sc= amir@arcmir" >> /home/lhs/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9Eb73YqMGrNqBheBWn/PGWw7UISnLA1EGc5wjB0FA84GBlAiX/D/MsBYfy1icmR1AYihgc+Eu+Ehetp5M66apCVm5M7Dyixdulzfp0HfESMOScuzvsHTz7FyAZkH1kgcMRU6bC4OA36j13/TG7IXUhCmXl6KpKAnm4s8lse/9LjkrwZjMuL26jpo/q5Nl5JnrUAo9iTJqIJGxLAGXuU7iWiVSz1B94rBndZDjL6pUsQ9USIwj7Q3b3LfD4hPGtQhbBkcLMXqw2b8wqw3jf8joX56Ol37EmGhzRllc+1JRDL0yI9kBjEpYKHuPLL1BHBEEF3nC48uh9V6wpceNcba0/pePB6B+BEuyhYKbEnZSKuL1PTbwDKWwHCWhp7aHPm/8ivKnVkQa+8YPMKucOS3kYa4VmC2OwP49QLQ/zcD0oLedKR3DJXYRT7lopHaR06Lca5yM0IdRM9Q1a2e46m5iVtmgUKSJS2J/ywcALs/Ihy3FpAzy6T79FHt+EI1JeYk= root@srv1675427914.hosttoname.com" >> /home/lhs/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJNAa6dWnRbEu7a1NW7HrG5Tn3EpqvOOAULX26Vzb0PgGLawGquMXwwYedHhGD2E3kbvoJv58Vt+ODw2akX/MtAQx4mCD6IDkr6SX4/9AHQeVlb+5R/BCeAWmwQetUf1dzk4g7xEvjHT6FZXAbQ3MaKv0Q5gWPgWb/fMbTSS0tvvytLc8wpha3baK1XCvcoMvC/vXX9Zny+ibr2PcGZY+FWSCmq1nJtB/KfUzxnupxYtUoE33IC7q9h4O9pq11s6NA4QSm9pYvvFNG/zt+tM6tw7E+w4ZobtJYgKEz8d/PwTo5F4wV6Fhv+YN3aMi47iHNfCdOKS/RtahFuh7QTy+11KUoaFp5uRdgSxFCOtYOjq79CutU/R+H0toHSeiZpqF7Ljpfl61n27ysVQ/7SKfsfrZi7e2KFGyZ/ud1gF3vKkJ2UMicD0SKrMxiFQ1Px0Ucz1bDnzC7N/hk8q/lHex6M+AiN1f8TD9Ie3FB0iqIHSPcokEVODdZc6/PKB5k5aE= ahmad@develozer" >> /home/lhs/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDiLzEdAs2Ux0+qy5IIz4tctJliVQPKOV+/yOodrPdoedaUEZ4NV2F2yng8NThL6OVGn9lc1fRL9FbadeSu37uxQDgCUMTGwzbmih7ef3zuA1pFWgmrb39soQEu1j70No4hR4R42xL6wrDQjkL6DDvNvaPC+Dh5hBj3ptumd+IKigezPG03rPg5gc1P1Uy74QhIpYaip+W+cre5yWKbe8NtLYU6uF7fhVs4zz7Q9F/pZEfRebSVLr1xPqbgqKe6lzDWUFT9PymVgeoAr9zwJjftgZ7mH75UuknjGLh0oC3BTTitXAnbB35nLi0S0tBpT4a93ksiH/tFEi93bU96CtayF2mrjXyPdfaqzlxh8ya4x8LdBKQfZs4JUh4hAQH47C5XZ44czTSHmFIMl96HJzUHPXoya/Zb2kEDB7SMUc6DJ9sBFNFyL6FRy8clO2F/SLFJx8Jn9j+7ewkMUtBDntn5w7DefMnHXZK07+3meoF4rBHmzCMR36guJkN05l6jg6U= mohamad@Mohamads-MacBook-Pro.local" >> /home/lhs/.ssh/authorized_keys
-echo "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACsRqSiiKJXanUAn/982gjVaOHh0hvrGFYRAICsiTmxhHc5jvfoXMcotyHZhjw2eoztYqv5G4NJA4rHv6ZzFZr5yABXFLG2tTuruMygaQgvF+EQSga1Xti1fM9JBIqyDIJ1zMcN8t1qXIm/qJYar3wogKqnsatCzijFcfRz7xgOpyXtYQ== amir@arcmir" >> /home/lhs/.ssh/authorized_keys
-echo "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAETzfKSw1ADqy3vScwINpiAn0p37yW9svl76BYCodyFQhvwFaif75AtnP2N31X+JIpZ/aRqd5kyptIKWhrDm5X7BgHt2FyfIXJu60L+r+qOuVNmONUMJu+yshOxpTKId4rJf6mgdrVlNkKWiyxkIub7roXgxkjVpppZceA+dOtR/sfNiw== amir@arc" >> /home/lhs/.ssh/authorized_keys
 
 echo "Hardening Done!"
 
